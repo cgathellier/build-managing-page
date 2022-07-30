4050ca4f35a992e93ff2e286f434428da7771180
Clemzouz <clement.gathellier.dev@gmail.com>
2022-07-30 19:24:17 +0200

    retour à la première slide quand click sur la dernière

--- dist/scripts/mobile-slider-animation/Slider.js
+++ dist/scripts/mobile-slider-animation/Slider.js
@@ -1,17 +1,31 @@
 "use strict";
 class Slider {
     constructor() {
+        var _a;
         this.$slider = document.querySelector('.testimonials__slider');
+        this.$slides = document.querySelectorAll('.testimonials__slide');
+        this.numberOfSlides = ((_a = this.$slides) === null || _a === void 0 ? void 0 : _a.length) || 0;
         this._currentSlide = 1;
         this._prevSlide = 4;
         this._nextSlide = 2;
         this.listen();
+        this.prevSlide(this.numberOfSlides);
+        this.currentSlide(1);
+        this.nextSlide(2);
+    }
+    prevSlide(slideNum) {
+        this._prevSlide = slideNum;
     }
     currentSlide(slideNum) {
         this._currentSlide = slideNum;
     }
     nextSlide(slideNum) {
-        this._nextSlide = slideNum;
+        if (slideNum > this.numberOfSlides) {
+            this._nextSlide = 1;
+        }
+        else {
+            this._nextSlide = slideNum;
+        }
     }
     updateInterface() {
         var _a, _b;
--- src/typescript/mobile-slider-animation/Slider.ts
+++ src/typescript/mobile-slider-animation/Slider.ts
@@ -1,11 +1,21 @@
 class Slider {
 	private $slider = document.querySelector('.testimonials__slider');
+	private $slides: NodeListOf<HTMLElement> | null =
+		document.querySelectorAll('.testimonials__slide');
+	private numberOfSlides = this.$slides?.length || 0;
 	private _currentSlide: number = 1;
 	private _prevSlide: number = 4;
 	private _nextSlide: number = 2;
 
 	constructor() {
 		this.listen();
+		this.prevSlide(this.numberOfSlides);
+		this.currentSlide(1);
+		this.nextSlide(2);
+	}
+
+	prevSlide(slideNum: number) {
+		this._prevSlide = slideNum;
 	}
 
 	currentSlide(slideNum: number) {
@@ -13,7 +23,11 @@ class Slider {
 	}
 
 	nextSlide(slideNum: number) {
-		this._nextSlide = slideNum;
+		if (slideNum > this.numberOfSlides) {
+			this._nextSlide = 1;
+		} else {
+			this._nextSlide = slideNum;
+		}
 	}
 
 	updateInterface() {
